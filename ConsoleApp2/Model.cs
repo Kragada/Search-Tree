9c40df8ae262ed9bd0964d050db2663c220e6639
Kragada <35291972+Kragada@users.noreply.github.com>
2021-10-19 13:32:01 +0200

    Add project files.

---
+++ ConsoleApp2/Model.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Model
+    {
+        private Node rootNode;
+
+        public Model()
+        {
+            // Code for setting up testing base, this sould be removed or changed later
+            this.setRootNode(new Node(0));
+            this.addNewNodeToExistingNode(rootNode, new Node(5), "right");
+            this.addNewNodeToExistingNode(rootNode, new Node(-2), "left");
+            Console.WriteLine(rootNode.toString());
+
+        }
+
+        // Method for controlling the flow of the program
+        private void ProgramFlow()
+        {
+            bool running = true;
+            while (running)
+            {
+
+            }
+        }
+
+        // Adding new node to specific position
+        private void addNewNodeToExistingNode(Node existingNode, Node newNode, String direction)
+        {
+            if (direction == "left")
+            {
+                existingNode.setLeftNode(newNode);
+            }
+            else
+            {
+                existingNode.setRightNode(newNode);
+            }
+        }
+
+        // Method for add new nodes based on search tree rules
+        private void addNewNode(Node newNode)
+        {
+
+        }
+
+        // Recursive method for printing all nodes
+        private void printAllNodes()
+        {
+
+        }
+
+        // Method for finding specific node by value
+        private Node FindNodeByValue(int searchValue)
+        {
+            return null;
+        }
+
+        private void setRootNode(Node newRootNode)
+        {
+            this.rootNode = newRootNode;
+        }
+    }
+}
